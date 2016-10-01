7cb2aa0e8e22f6872b0e042bda8e177599cfd592
Randall Porter <porterrandall@yahoo.com>
2016-10-01 10:42:34 -0700

    lab1

---
+++ lab1/lab1/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using lab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public ActionResult Index()
+        {
+            Person person = new Person();
+            person.Name = "I am a Person yo, I real person.";
+            ViewBag.Message = person.Name;
+            return View();
+        }
+    }
+}
---
+++ lab1/lab1/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab1.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public decimal Occupation { get; set; }
+    }
+}
